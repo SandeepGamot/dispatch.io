a8e24db0f02da8b8c2f7953500c91009f170ee5a
Sandeep Gamot <sandeep@shortcastle.com>
2021-06-18 02:17:38 +0530

    [sg]: add header with home navigation for all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import Title from "antd/lib/typography/Title";
 import React from "react";
 import DownloadPage from "./pages/DownloadPage";
 import Home from "./pages/Home";
 
 function App() {
-  return window.location.pathname.includes("/files") ? (
-    <DownloadPage />
-  ) : (
-    <Home />
+  return (
+    <>
+      <div className="ml-4 mt-4 p-4">
+        <span
+          className="p-2 font-black text-2xl select-none cursor-pointer hover:text-gray-500"
+          onClick={() => {
+            window.location.pathname = "";
+          }}
+        >
+          Dispatch.io
+        </span>
+      </div>
+      {window.location.pathname.includes("/files") ? (
+        <DownloadPage />
+      ) : (
+        <Home />
+      )}
+    </>
   );
 }
 
